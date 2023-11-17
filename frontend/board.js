bc0d8464fbda07d5e7f8e356dba2a563cdcdb35f
jackrankin <97968661+jackrankin@users.noreply.github.com>
2023-11-17 12:48:40 -0600

    made the go module

--- frontend/board.js
+++ frontend/board.js
@@ -1,6 +1,9 @@
 
 const arr = new Array();
 const a = new Set();
+const visited = new Set(); // TODO
+const words = new Array();
+
 var x=0,y=0;
 var typing=0;
 
@@ -11,8 +14,7 @@ var left=0;
 var up=0;
 var keyCounter=0;
 
-function getLetters(){ // in reality this should read from the DB
-
+function getLetters(){
     for (var i = 0; i < 16; i++) {
         var letter = String.fromCharCode('A'.charCodeAt() + Math.floor(Math.random() * 26));
 
@@ -23,57 +25,64 @@ function getLetters(){ // in reality this should read from the DB
         arr.slice(-1)[0].push(letter);
         document.getElementById("hitbox-"+i.toString()).innerText = letter;
     }
-
 }
 
 getLetters();
 
 function resetKeys(){
+
     up=0;
     down=0;
     left=0;
     right=0;
+
 }
 
 function checkWord(){
+    
     console.log(word);
-    word="";
     document.getElementById("currentWord").innerText = word;
-    for (let i = 0; i < 16; i++) {
 
+    for (let i = 0; i < 16; i++) {
         document.getElementById("cell-" + (i).toString()).style.backgroundColor = "white";
     }
+
+    words.push(word);
+    word="";
+    visited.clear();
+    document.getElementById("wList").innerText = words.join(" ")
+
 }
 
 
-function makeMove(){
 
-    let ch=0;
+function makeMove(){
+    let prev_x=x, prev_y=y;
+
     if (up && y > 0) {
-        ch=1;
         y--;
     }
 
     if (down && y < 3){
-        ch=1;
         y++;
     }
 
     if (right && x < 3){
-        ch=1;
         x++;
     }
 
     if (left && x > 0) {
-        ch=1;
         x--;
     }
-
-
-    word += document.getElementById("hitbox-" + ((4 * y) + x).toString()).innerText;
-    
-    document.getElementById("currentWord").innerText = word;
-    document.getElementById("cell-" + ((4 * y) + x).toString()).style.backgroundColor = "red";
+    if (visited.has((4*y)+x)){
+        x=prev_x;
+        y=prev_y;
+    } else {
+        visited.add((4*y)+x);
+        word += document.getElementById("hitbox-" + ((4 * y) + x).toString()).innerText;
+        document.getElementById("currentWord").innerText = word;
+        document.getElementById("cell-" + ((4 * y) + x).toString()).style.backgroundColor = "red";
+    }
 
 }
 
