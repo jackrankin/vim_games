c709ba869d3d03ac903dfce79206f44d082f9abb
jackrankin <97968661+jackrankin@users.noreply.github.com>
2023-11-19 21:06:15 -0600

    messed around with chi endpoints

---
+++ backend/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+    server();
+}
+
+
+func server(){
+    r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+
+	r.Get("/a", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("a"))
+	})
+	
+	r.Get("/b", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("b"))
+	})
+
+    http.ListenAndServe(":3000", r)
+}
